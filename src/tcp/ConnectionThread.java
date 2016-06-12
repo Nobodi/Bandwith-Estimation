aa75b602025670762119e97994f6b175970422c1
Nobodi <michael.jokic@stud.uni-due.de>
2016-06-12 11:00:16 +0200

    Add Timestamps to Logger; Database Connection

--- src/tcp/ConnectionThread.java
+++ src/tcp/ConnectionThread.java
@@ -7,11 +7,18 @@ import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Locale;
 
 import org.apache.log4j.Logger;
+
+import com.google.gson.JsonSyntaxException;
 
 public class ConnectionThread extends Thread
 {
@@ -21,26 +28,45 @@ public class ConnectionThread extends Thread
 	private int factor;
 	private int packetSize;
 	private int ptPackets;
+	private String rtt, download;
+	private int bufferSize;
 
-	public ConnectionThread(Socket client, Logger logger, int packetSize)
+	public ConnectionThread(Socket client, Logger logger, int packetSize,
+			String rtt, String download)
 	{
 		this.client = client;
 		this.logger = logger;
 		this.packetSize = packetSize;
 		this.rate = 0;
-		this.factor = 500000;
+		this.factor = 800000;
+		this.rtt = rtt;
+		this.bufferSize = 0;
+	}
+
+	public static String getActualDate()
+	{
+		// Get exakt Date
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat dt = new SimpleDateFormat("d.M.yyyy,HH.mm.ss",
+				Locale.US);
+		DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.MEDIUM);
+		return dt.format(cal.getTime());
 	}
 
 	@Override
 	public void run()
 	{
 		String clientIP = client.getInetAddress().getHostAddress();
-		System.out.println("Neue Verbindung " + clientIP);
+		System.out.println("Neue Verbindung " + clientIP + ". - "
+				+ ConnectionThread.getActualDate());
 
-		logger.info("Connection: New Connection from " + clientIP + ".");
+		logger.info("Connection: New Connection from " + clientIP + ". - "
+				+ ConnectionThread.getActualDate());
 
 		try
 		{
+			this.bufferSize = client.getReceiveBufferSize();
+
 			BufferedReader in = new BufferedReader(new InputStreamReader(
 					client.getInputStream()));
 
@@ -51,20 +77,7 @@ public class ConnectionThread extends Thread
 
 			while ((line = in.readLine()) != null)
 			{
-				if (line.equals("Spruce"))
-				{
-					char[] a = new char[packetSize];
-					Arrays.fill(a, 'x');
-					String message = new String(a);
-					out.write(message + "\n");
-					out.flush();
-
-					Thread.sleep(200);
-
-					out.write(message + "\n");
-					out.flush();
-				} else if (line.startsWith("PacketTrain")
-						|| line.startsWith("Value"))
+				if (line.startsWith("PacketTrain") || line.startsWith("Value"))
 				{
 					if (line.startsWith("PacketTrain"))
 					{
@@ -74,7 +87,7 @@ public class ConnectionThread extends Thread
 						System.out.println("Paketlaenge: " + this.packetSize);
 						System.out.println("Anzahl Pakete: " + this.ptPackets);
 						logger.info("PacketTrain requested from " + clientIP
-								+ ".");
+								+ ". - " + ConnectionThread.getActualDate());
 					}
 					if (line.startsWith("Value"))
 					{
@@ -121,35 +134,110 @@ public class ConnectionThread extends Thread
 				{
 					out.write(rate + "\n");
 					out.flush();
+				} else if (line.equals("Download"))
+				{
+					System.out.println("Download erkannt");
+					logger.info("Download requested from " + clientIP + ". - "
+							+ ConnectionThread.getActualDate());
+
+					for (int i = 0; i < 40; i++)
+					{
+						out.write(rtt);
+						out.flush();
+					}
+					out.write("\nEnd\n");
+					out.flush();
+
+				} else if (line.equals("RTT"))
+				{
+					System.out.println("RTT erkannt");
+					logger.info("RTT requested from " + clientIP + ". - "
+							+ ConnectionThread.getActualDate());
+
+					out.write(rtt);
+					out.write("\nEnd\n");
+					out.flush();
+					System.out.println("Done.");
+				} else if (line.equals("data"))
+				{
+					System.out.println("New Data Arrived!!!");
+
+					logger.info("New Data for Storage from " + clientIP
+							+ ". - " + ConnectionThread.getActualDate());
+
+					String temp = "";
+					String message = "";
+					while ((temp = in.readLine()) != null)
+					{
+						System.out.println(temp);
+						message = message + temp;
+						if (temp.equals("End"))
+						{
+							System.out.println("BREAK");
+							break;
+						}
+					}
+					message = message.replace("End", "");
+
+					// Add new Data to Database
+					AddResultstoDatabase add = new AddResultstoDatabase();
+					ArrayList<Results> results = add
+							.getObjectsFromString(message);
+					add.putDataintoDatabase(results);
+					System.out.println("Data saved.");
+
 				} else
 				{
+					client.setReceiveBufferSize(1);
+					client.setSendBufferSize(1);
 					out.write(line + "\n");
 					out.flush();
+					client.setReceiveBufferSize(this.bufferSize);
+					client.setSendBufferSize(this.bufferSize);
 					logger.info("Message (" + line.length() + "Bytes) from "
-							+ clientIP + " arrived.");
+							+ clientIP + " arrived. - "
+							+ ConnectionThread.getActualDate());
 				}
 			}
 
 			client.close();
-			System.out.println("Verbindung mit " + clientIP + " beendet.");
-			logger.info("Connection: Connection with " + clientIP + " closed.");
+			System.out.println("Verbindung mit " + clientIP + " beendet. - "
+					+ ConnectionThread.getActualDate());
+			logger.info("Connection: Connection with " + clientIP
+					+ " closed. - " + ConnectionThread.getActualDate());
 		} catch (SocketException e)
 		{
-			logger.error("Socket from " + clientIP
-					+ " interrupted. Error Message: " + e.getMessage());
-			System.out.println("SocketException: Verbindung unterbrochen.");
+			logger.error("Socket from " + clientIP + " interrupted. - "
+					+ ConnectionThread.getActualDate() + " - Error Message: "
+					+ e.getMessage());
+			System.out.println("SocketException: Verbindung unterbrochen. - "
+					+ ConnectionThread.getActualDate());
 		} catch (SocketTimeoutException e)
 		{
-			logger.error("Socket Timeout from " + clientIP + ".");
+			logger.error("Socket Timeout from " + clientIP + ". - "
+					+ ConnectionThread.getActualDate());
 			System.out
-					.println("SocketTimeoutException: Client antwortet nicht.");
-		} catch (InterruptedException e)
+					.println("SocketTimeoutException: Client antwortet nicht. - "
+							+ ConnectionThread.getActualDate());
+		} catch (SQLException e)
 		{
-			logger.error("Problem with Thread. Error Message: "
+			logger.error("SQLException: Problem to save Data to Database. - "
+					+ ConnectionThread.getActualDate() + " - Error Message: "
 					+ e.getMessage());
+			System.out
+					.println("SQLException: Problem to save Data to Database. - "
+							+ ConnectionThread.getActualDate());
+		} catch (JsonSyntaxException e)
+		{
+			logger.error("Problem with Data Syntax. Not compatible Data. - "
+					+ ConnectionThread.getActualDate());
+			System.out
+					.println("Problem with Data Syntax. Not compatible Data. - "
+							+ ConnectionThread.getActualDate());
 		} catch (Exception e)
 		{
-			logger.error("General Error. Error Message: " + e.getMessage());
+			logger.error("General Error. - " + ConnectionThread.getActualDate()
+					+ " - Error Message: " + e.getMessage());
 		}
 
 	}
